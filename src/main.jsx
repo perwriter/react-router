7b8902d62588491ce6a4515665c51eb75f58aa6d
perwriters <perwriters@gmail.com>
2023-11-27 18:44:35 +0300

    Trying to following a log the react router documentation

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+import Root from "./routes/root";
+
+import ErrorPage from "./error-page";
+
+import Contact from "./routes/contact";
+
+// import Root, { loader as rootLoader } from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    // loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
